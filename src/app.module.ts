811941c2cb675f291bf0f895dd22c7f3a9d14d3c
Robson Carvalho <robsoncarvalho.tech@gmail.com>
2023-11-27 10:42:42 -0300

    feat: user utilities

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { UsersModule } from './app/users/users.module';
 
 @Module({
   imports: [
@@ -12,9 +13,10 @@ import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
       username: process.env.PG_USER,
       password: process.env.PG_PASSWORD,
       database: process.env.PG_DB,
-      entities: [__dirname + '/**/*.entity{.ts,.js'],
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
     } as TypeOrmModuleOptions),
+    UsersModule,
   ],
   controllers: [],
   providers: [],
---
+++ src/helpers/message.helper.ts
@@ -0,0 +1,4 @@
+export const MessagesHelper = {
+  PASSWORD_VALID:
+    'A senha deve conter letras maiúscilas, minúsculas, números e caracteres especiais',
+};
