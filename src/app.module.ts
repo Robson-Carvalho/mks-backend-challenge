1bd58adc095c5d8e065a0e9a27f032fa2c9b8a07
Robson Carvalho <robsoncarvalho.tech@gmail.com>
2023-11-26 22:33:27 -0300

    chore: reset configuration project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: process.env.DB_TYPE,
+      host: process.env.PG_HOST,
+      port: process.env.PG_PORT,
+      username: process.env.PG_USER,
+      password: process.env.PG_PASSWORD,
+      database: process.env.PG_DB,
+      entities: [__dirname + '/**/*.entity{.ts,.js'],
+      synchronize: true,
+    } as TypeOrmModuleOptions),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
