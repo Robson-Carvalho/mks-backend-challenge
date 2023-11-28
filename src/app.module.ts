c4f426e6939cbdb2b9e399bc3a6b230a80193609
Robson Carvalho <robsoncarvalho.tech@gmail.com>
2023-11-28 16:11:07 -0300

    feat: used service railway

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,12 +11,12 @@ import { MoviesModule } from './app/movies/movies.module';
     TypeOrmModule.forRootAsync({
       useFactory: () =>
         ({
-          type: 'postgres',
-          host: process.env.RAILWAY_PG_HOST,
-          port: parseInt(process.env.RAILWAY_PG_PORT, 10),
-          username: process.env.RAILWAY_PG_USER,
-          password: process.env.RAILWAY_PG_PASSWORD,
-          database: process.env.RAILWAY_PG_DB,
+          type: process.env.DB_TYPE,
+          host: process.env.PG_HOST,
+          port: parseInt(process.env.PG_PORT, 10),
+          username: process.env.PG_USER,
+          password: process.env.PG_PASSWORD,
+          database: process.env.PG_DB,
           entities: [__dirname + '/**/*.entity{.ts,.js}'],
           synchronize: true,
         }) as TypeOrmModuleOptions,
